28de4863cd0f71e9bd3e247d7d6d38b1aa4b19d1
Yazmin Perez <zoeperez333@gmail.com>
2022-04-19 20:52:06 -0700

    test 2-6 passing

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -7,5 +7,30 @@ describe(Coins) do
       coin = Coins.new()
       expect(coin.coin_counter(0.25)).to(eq([1, 0, 0, 0]))
     end
+
+    it('it should return quarters and dimes') do
+      coin2 = Coins.new()
+      expect(coin2.coin_counter(0.10)).to(eq([0, 1, 0, 0]))
+    end
+
+    it('it should return quarters and dimes') do
+      coin2 = Coins.new()
+      expect(coin2.coin_counter(0.05)).to(eq([0, 0, 1, 0]))
+    end
+
+    it('it should return quarters and dimes') do
+      coin2 = Coins.new()
+      expect(coin2.coin_counter(0.01)).to(eq([0, 0, 0, 1]))
+    end
+
+    it('it should return quarters and dimes') do
+      coin2 = Coins.new()
+      expect(coin2.coin_counter(0.41)).to(eq([1, 1, 1, 1]))
+    end
+
+    it('it should return quarters and dimes') do
+      coin2 = Coins.new()
+      expect(coin2.coin_counter(5.16)).to(eq([20, 1, 1, 1]))
+    end
   end
 end
